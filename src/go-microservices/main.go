5dcdf1aac9272bc18d78af52a8b064246faa5b7a
rashed-k <rashedkhan.e36@gmail.com>
2022-09-14 20:06:26 +1000

    go-app-commit

---
+++ src/go-microservices/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	x := 10
+	name := "DevOps"
+	isworking := false
+	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
+	fmt.Println(x, name, isworking)
+}
